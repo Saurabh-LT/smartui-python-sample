0e017ca81ef0f7b7864fdc6be9a07470d98a228f
Saurabh <saurabh@lambdatest.com>
2023-03-29 18:12:00 +0530

    print access and hub

--- test.py
+++ test.py
@@ -40,6 +40,8 @@ class FirstSampleTest(unittest.TestCase):
         driver = self.driver
 
         # Url
+        print("Hub " + hub)
+        print("Access Key " + access_key)
         driver.get("https://lambdatest.com")
         print("Taking screenshot")
         driver.execute_script("smartui.takeScreenshot,{\"screenshotName\":\"sample-screenshot-1\"}")
