0979a60602b43f1aaf92da912ab294c7297ddcf0
Saurabh <saurabh@lambdatest.com>
2024-10-18 14:58:32 +0530

    Added code

--- SmartUI_SDK_LT_hub.py
+++ SmartUI_SDK_LT_hub.py
@@ -1,17 +1,28 @@
 from lambdatest_selenium_driver import smartui_snapshot
 import unittest
 from selenium import webdriver
+from selenium.webdriver.safari.options import Options as SafariOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
 
 username = os.getenv("LT_USERNAME")  # Replace the username
 access_key = os.getenv("LT_ACCESS_KEY")  # Replace the access key
+browser_name = os.getenv("BROWSER_NAME", "chrome")  # Replace the access key
+
+
 class FirstSampleTest(unittest.TestCase):
     def setUp(self):
-        options = webdriver.ChromeOptions()
-        options.browser_version = "118.0"
-        options.platform_name = "Windows 11"
+        if browser_name.lower() == "safari":
+            options = SafariOptions()
+        elif browser_name.lower() == "chromiumedge" or browser_name.lower() == "edge":
+            options = webdriver.EdgeOptions()
+        elif browser_name.lower() == "firefox":
+            options = webdriver.FirefoxOptions()
+        else:
+            options = webdriver.ChromeOptions()
+        options.browser_version = "latest"
+        options.platform_name = "macOS Sonoma"
         lt_options = {};
         lt_options["resolution"] = "1024x768";
         lt_options["project"] = "Python_SDK";
@@ -23,12 +34,14 @@ class FirstSampleTest(unittest.TestCase):
         lt_options["selenium_version"] = "4.0.0"
         options.set_capability('LT:Options', lt_options);
         self.driver = webdriver.Remote(
-            command_executor="http://{}:{}@hub.lambdatest.com/wd/hub".format(
+            command_executor="http://{}:{}@stage-hub.lambdatestinternal.com/wd/hub".format(
                 username, access_key),
             options=options
         )
+
     def tearDown(self):
         self.driver.quit()
+
     def test_demo_site(self):
         try:
             driver = self.driver
@@ -36,13 +49,14 @@ class FirstSampleTest(unittest.TestCase):
             driver.set_page_load_timeout(30)
             print('Loading URL')
             driver.get("https://www.lambdatest.com/")
-            smartui_snapshot(driver,"1st SS")
+            smartui_snapshot(driver, "1st SS")
             print("1st screenshot")
             driver.implicitly_wait(10)
             driver.execute_script("lambda-status=passed")
         except:
             driver.execute_script("lambda-status=failed")
             print("Failed")
-            
+
+
 if __name__ == "__main__":
     unittest.main()
